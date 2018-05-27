5dabc9dec3b3c9a5e42ef305d9d88a522339023f
Shail Shah <shahshail9898@gmail.com>
2018-05-27 00:42:48 -0400

    Initial commit

---
+++ app/src/main/java/music/tiohome/com/kotlinyoutubeplayer/MainActivity.kt
@@ -0,0 +1,16 @@
+package music.tiohome.com.kotlinyoutubeplayer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+    }
+}
