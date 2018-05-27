b4657dec400c76f48df36bfc1817cedc6e2db41c
Shail Shah <shahshail9898@gmail.com>
2018-05-27 01:06:00 -0400

    Layout Created

--- app/src/main/java/music/tiohome/com/kotlinyoutubeplayer/MainActivity.kt
+++ app/src/main/java/music/tiohome/com/kotlinyoutubeplayer/MainActivity.kt
@@ -12,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.adapter =
+        recycler_view.adapter = MainAdapter()
+
+
 
     }
 }
