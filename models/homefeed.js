8a8446ba4e1bf924127389cd76e18e7e2b2a0f0b
Shail Shah <shahshail9898@gmail.com>
2018-07-16 15:31:56 -0400

    Joi Validation added

--- models/homefeed.js
+++ models/homefeed.js
@@ -1,6 +1,88 @@
-// const Joi = require('joi')
-// const mongoose = require('mongoose')
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const {userSchema} = require('./user');
 
-// const HomeFeed= mongoose.model('home_feed', new mongoose.Schema({
-    
-// })) 
+//Channel Schema
+const ChannelSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlength : 500
+    },
+    profileImageUrl: {
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlength : 500
+    },
+    numberOfSubscribers : {
+        type : Number,
+        required : true
+    }
+},{_id : false});
+
+//Videos Schema
+const Videos = new mongoose.Schema({
+    id : {
+        type : Number,
+        minlength : 1,
+        maxlength : 100000,
+        required : true
+    },
+    name: {
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlength : 500
+    },
+    link: {
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlength : 500
+    },
+    imageUrl: {
+        type : String,
+        required : false,
+        minlength : 3,
+        maxlength : 500
+    },
+    numberOfSubscriber : {
+        type : Number,
+        minlength : 1,
+        maxlength : 100000,
+        required : true
+    },
+    channel : {
+        type : ChannelSchema,
+        required: true
+    }
+
+},{_id : false})
+
+
+
+
+const HomeFeed = mongoose.model('HomeFeed', new mongoose.Schema({
+    user : {
+        type : userSchema,
+        required : true
+    },
+    videos : [{
+        type : Videos,
+        required : true
+    }]
+}));
+
+function validateHomeFeed(homeFeed) {
+  const schema = {
+    user: Joi.required(),
+    videos : Joi.required()
+  };
+
+  return Joi.validate(homeFeed, schema);
+}
+
+exports.HomeFeed = HomeFeed; 
+exports.validateHomeFeed = validateHomeFeed;
