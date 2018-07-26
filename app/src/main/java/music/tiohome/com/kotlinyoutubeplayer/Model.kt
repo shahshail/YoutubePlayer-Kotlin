dcb5521e14de525e2653555f4b7ef3c8df8b7e0e
Shail Shah <shahshail9898@gmail.com>
2018-07-26 00:38:18 -0400

    Refactor/Cleamup

--- app/src/main/java/music/tiohome/com/kotlinyoutubeplayer/Model.kt
+++
@@ -1,7 +0,0 @@
-package music.tiohome.com.kotlinyoutubeplayer
-
-object Model {
-    data class Result(val query: Query)
-    data class Query(val searchinfo: SearchInfo)
-    data class SearchInfo(val totalhits: Int)
-}
